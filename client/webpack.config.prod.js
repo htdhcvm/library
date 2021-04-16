e33695704a861af01fb8d0406f63dfe9c803a41b
flouheforst <flouheforst@gmail.com>
2021-04-16 14:20:03 +0300

    for production

--- client/webpack.config.prod.js
+++ client/webpack.config.prod.js
@@ -1,3 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
+    entry: ['babel-polyfill', path.resolve(__dirname, 'src/index.jsx')],
+    devtool: 'inline-source-map',
     mode: 'production',
+    output: {
+        path: path.resolve(__dirname, 'dist/'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            '@axios': path.resolve(__dirname, 'helpers/axiosService'),
+            '@features': path.resolve(__dirname, 'src/features'),
+            '@assets': path.resolve(__dirname, 'assets'),
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|jpg|gif|svg|gif)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                    },
+                ],
+            },
+        ],
+    },
+
+    // plugins: [new HtmlWebpackPlugin()],
 };
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const app = require('./app');
+const express = require('express');
+const path = require('path');
+
+app.use(express.static(`${__dirname}/client/dist`));
 
 require('./config')(app);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/client/dist/index.html`));
+});
 
 app.listen(process.env.PORT, () =>
     console.log(`Server has been started on ${process.env.PORT}`)
